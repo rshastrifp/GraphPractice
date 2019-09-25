afb38be1911000ea97b62b61bcff9cb4e77bd141
Ronak Shastri <ronak.shastri@scotiabank.com>
2019-09-25 16:31:00 -0400

    Initial Commit

---
+++ GraphPractice/GraphPractice.h
@@ -0,0 +1,19 @@
+//
+//  GraphPractice.h
+//  GraphPractice
+//
+//  Created by Ronak Shastri on 2019-09-25.
+//  Copyright © 2019 Ronak Shastri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GraphPractice.
+FOUNDATION_EXPORT double GraphPracticeVersionNumber;
+
+//! Project version string for GraphPractice.
+FOUNDATION_EXPORT const unsigned char GraphPracticeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GraphPractice/PublicHeader.h>
+
+
